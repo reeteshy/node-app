bbe940a06500a94349692dd2d8ae97e553a6ab84
Reetesh Yadav <reeteshy15@gmailcom>
2024-09-21 17:35:23 +0530

    added static port

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 require('dotenv').config()
 
+const port = 5000;
 const app = express()
 
 app.get('/', (req, resp)=>{
     return resp.send({"name":"Reetesh Yadav", "age":30})
 })
 
-app.listen(process.env.PORT, ()=>{
+app.listen(port, ()=>{
     console.log("Application runnning on port ", process.env.PORT)
 })
